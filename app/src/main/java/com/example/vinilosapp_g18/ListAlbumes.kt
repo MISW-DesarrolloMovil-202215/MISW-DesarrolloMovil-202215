8d760892ca3dc214f64a6a279213e4dd6a6d81f4
Andres De la Hoz <andelahozg@gmail.com>
2022-11-05 17:29:42 -0500

    Actualizar listar albumes

--- app/src/main/java/com/example/vinilosapp_g18/ListAlbumes.kt
+++ app/src/main/java/com/example/vinilosapp_g18/ListAlbumes.kt
@@ -2,10 +2,21 @@ package com.example.vinilosapp_g18
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
 class ListAlbumes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_albumes)
+        val arrayAdapter: ArrayAdapter<*>
+
+        val albumes = mutableListOf("Album 1", "Album 2", "Album 3", "Album 4", "Album 5", "Album 6")
+        val lvAlbumes = findViewById<ListView>(R.id.lvAlbumes)
+
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, albumes)
+        lvAlbumes.adapter = arrayAdapter
+
     }
 }
