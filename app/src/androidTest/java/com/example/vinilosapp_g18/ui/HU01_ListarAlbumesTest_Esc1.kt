d94fc6e3450c2b39479d91b4d7494f8cdcdbf3ad
Andres De la Hoz <andelahozg@gmail.com>
2022-11-06 20:55:09 -0500

    Pruebas E2E listarAlbumes implementadas

---
+++ app/src/androidTest/java/com/example/vinilosapp_g18/ui/HU01_ListarAlbumesTest_Esc1.kt
@@ -0,0 +1,149 @@
+package com.example.vinilosapp_g18.ui
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.vinilosapp_g18.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class ListarAlbumesTest_Esc1 {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun listarAlbumesTest() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.example.vinilosapp_g18", appContext.packageName)
+
+        Thread.sleep(2000)
+        val linearLayout = onView(
+            allOf(
+                withId(R.id.hometext),
+                withParent(withParent(withId(android.R.id.content))),
+                isDisplayed()
+            )
+        )
+        linearLayout.check(matches(isDisplayed()))
+
+        val textView = onView(
+            allOf(
+                withText("Vinilos"),
+                withParent(
+                    allOf(
+                        withId(R.id.hometext),
+                        withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Vinilos")))
+
+        val textView2 = onView(
+            allOf(
+                withText("Explorar el mundo a través de tus oidos."),
+                withParent(
+                    allOf(
+                        withId(R.id.hometext),
+                        withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("Explorar el mundo a través de tus oidos.")))
+
+        val linearLayout2 = onView(
+            allOf(
+                withId(R.id.menu),
+                withParent(withParent(withId(android.R.id.content))),
+                isDisplayed()
+            )
+        )
+        linearLayout2.check(matches(isDisplayed()))
+
+        val view = onView(
+            allOf(
+                withId(android.R.id.statusBarBackground),
+                withParent(IsInstanceOf.instanceOf(android.widget.FrameLayout::class.java)),
+                isDisplayed()
+            )
+        )
+        view.check(matches(isDisplayed()))
+
+        val albumsButton = onView(
+            allOf(
+                withId(R.id.albumes), withText("Albumes"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        albumsButton.perform(click())
+
+//        val backgroundMenu = onView(
+//            allOf(
+//                withId(R.id.splashtext), withText("Vinilos"),
+//                withParent(IsInstanceOf.instanceOf(android.widget.FrameLayout::class.java)),
+//                isDisplayed()
+//            )
+//        )
+
+        view.perform(click())
+
+        Thread.sleep(3000)
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.albumsRv),
+                withParent(withParent(withId(R.id.nav_host_fragment))),
+                isDisplayed()
+            )
+        )
+        recyclerView.check(matches((isDisplayed())))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
+
