cdd7929fc385b6b3ac42c1eaa7c287fdd2b78cfe
Andres De la Hoz <andelahozg@gmail.com>
2022-11-18 21:38:31 -0500

    Close #14

---
+++ app/src/androidTest/java/com/example/vinilosapp_g18/ui/HU03_ListarArtistasTest_Esc3.kt
@@ -0,0 +1,158 @@
+package com.example.vinilosapp_g18.ui
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.vinilosapp_g18.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class HU03_ListarArtistasTest_Esc3 {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun listarArtistasTest() {
+        val textView = onView(
+            allOf(
+                withText("Vinilos"),
+                withParent(
+                    allOf(
+                        withId(R.id.hometext),
+                        withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Vinilos")))
+        Thread.sleep(2000)
+        val textView2 = onView(
+            allOf(
+                withText("Vinilos"),
+                withParent(
+                    allOf(
+                        withId(R.id.hometext),
+                        withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(isDisplayed()))
+
+
+        val linearLayout = onView(
+            allOf(
+                withId(R.id.menu),
+                withParent(withParent(withId(android.R.id.content))),
+                isDisplayed()
+            )
+        )
+        linearLayout.check(matches(isDisplayed()))
+
+        val imageButton = onView(
+            allOf(
+                withId(R.id.artistas),
+                withParent(withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))),
+                isDisplayed()
+            )
+        )
+        imageButton.check(matches(isDisplayed()))
+
+        val button = onView(
+            allOf(
+                withId(R.id.artistas2), withText("ARTISTAS"),
+                withParent(withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))),
+                isDisplayed()
+            )
+        )
+        button.check(matches(isDisplayed()))
+
+        val viewGroup = onView(
+            allOf(
+                withParent(
+                    allOf(
+                        withId(android.R.id.content),
+                        withParent(withId(com.bumptech.glide.R.id.action_bar_root))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        viewGroup.check(matches(isDisplayed()))
+
+        val appCompatImageButton = onView(
+            allOf(
+                withId(R.id.artistas),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        1
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageButton.perform(click())
+        Thread.sleep(3000)
+        
+
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.artistRv),
+                isDisplayed()
+            )
+        )
+
+        for (i in 1..5){
+            recyclerView.check(matches((isDisplayed())))
+            Espresso.pressBack()
+            Thread.sleep(2000)
+            appCompatImageButton.perform(click())
+            Thread.sleep(2000)
+        }
+
+        recyclerView.check(matches((isDisplayed())))
+
+    }
+
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
