60bb2f2d49875e60bd15e51c09d19fce740a1a6b
JuanF998 <j.tamayoo@uniandes.edu.co>
2022-11-27 17:31:13 -0500

    Feature HU07 Crear Album (#69)
    * Close #27
    
    * Se crea diseño inicial para crear album
    
    * Close #26
    
    Co-authored-by: Julian_Acosta_Jaramillo <jacostaj@uniandes.edu.co>

--- app/src/main/java/com/example/vinilosapp_g18/ui/ListAlbumes.kt
+++ app/src/main/java/com/example/vinilosapp_g18/ui/ListAlbumes.kt
@@ -1,7 +1,11 @@
 package com.example.vinilosapp_g18.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import com.example.vinilosapp_g18.CreateAlbum
 import com.example.vinilosapp_g18.R
 
 
@@ -11,6 +15,21 @@ class ListAlbumes : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_albumes)
 
+        val btnCreateAlbum = findViewById<Button>(R.id.btnCreateAlbum)
+        btnCreateAlbum.setOnClickListener {
+            val intent = Intent(this, CreateAlbum::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val btnCreateAlbum2 = findViewById<ImageButton>(R.id.btnCreateAlbum2)
+        btnCreateAlbum2.setOnClickListener {
+            val intent = Intent(this, CreateAlbum::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
 
        // val albumes = mutableListOf("Album 1", "Album 2", "Album 3", "Album 4", "Album 5", "Album 6")
       //  val lvAlbumes = findViewById<ListView>(R.id.lvAlbumes)
@@ -19,4 +38,6 @@ class ListAlbumes : AppCompatActivity() {
         //lvAlbumes.adapter = arrayAdapter
 
     }
+
+
 }
