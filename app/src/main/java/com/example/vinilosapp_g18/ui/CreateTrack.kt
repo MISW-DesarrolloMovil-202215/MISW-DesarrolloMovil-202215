bf39596d160008404edbd3a54ee3aac56a46bca8
jc_local_pupo <arhuaco@gmail.com>
2022-11-28 19:38:19 -0500

    se agrega interfaz basica asociada a la HU

---
+++ app/src/main/java/com/example/vinilosapp_g18/ui/CreateTrack.kt
@@ -0,0 +1,15 @@
+package com.example.vinilosapp_g18
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateTrack : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_track)
+    }
+
+
+}
--- app/src/main/java/com/example/vinilosapp_g18/ui/ListAlbumes.kt
+++ app/src/main/java/com/example/vinilosapp_g18/ui/ListAlbumes.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import com.example.vinilosapp_g18.CreateAlbum
+import com.example.vinilosapp_g18.CreateTrack
 import com.example.vinilosapp_g18.R
 
 
@@ -31,7 +32,24 @@ class ListAlbumes : AppCompatActivity() {
 
 
 
-       // val albumes = mutableListOf("Album 1", "Album 2", "Album 3", "Album 4", "Album 5", "Album 6")
+        val btnCreateTrack = findViewById<Button>(R.id.btnCreateAlbum)
+        btnCreateTrack.setOnClickListener {
+            val intent = Intent(this, CreateTrack::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val btnCreateTrack2 = findViewById<ImageButton>(R.id.btnImgCreateTrack)
+        btnCreateTrack2.setOnClickListener {
+            val intent = Intent(this, CreateTrack::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+
+
+        // val albumes = mutableListOf("Album 1", "Album 2", "Album 3", "Album 4", "Album 5", "Album 6")
       //  val lvAlbumes = findViewById<ListView>(R.id.lvAlbumes)
 
        // arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, albumes)
